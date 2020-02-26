64601e8df41cc887c7154e0ec9bc93245d16bbdb
gabriela <gabipilegi@gmail.com>
2020-02-26 19:50:03 -0300

    Implementing clean logic

--- src/robot_cleaner/core.clj
+++ src/robot_cleaner/core.clj
@@ -2,6 +2,24 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "This program:
+  1. Reads standard inputs in the format:
+    ```
+    <number-of-commands>
+    <initial-x initial-y>
+    & <compass-direction> <number-of-steps>
+    ```
+    e.g:
+    ```
+    2
+    10 22
+    E 2
+    N 1
+    ```
+  2. Runs Clean Logic
+  3. Outputs standat out in the format:
+    ```
+    Cleaned: <number of unique cleaned positions>
+    ```"
   [& args]
-  (println "Hello, World!"))
+  (println "Not implemented"))
