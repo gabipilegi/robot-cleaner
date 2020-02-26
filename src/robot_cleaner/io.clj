4a63b516a9a898697784eb707b6c8b0986fc29b1
gabriela <gabipilegi@gmail.com>
2020-02-26 12:20:20 -0300

    Reorganize io file and add output fn with tests

--- src/robot_cleaner/io.clj
+++ src/robot_cleaner/io.clj
@@ -2,17 +2,22 @@
   (:require [clojure.string :as str]))
 
 (defn- read-int-ln []
-  (-> (read-line) Integer. int (+ 1)))
+  (-> (read-line)
+      Integer.
+      int
+      (+ 1)))
 
 (defn- input->str-list [number-of-lines]
-  (take number-of-lines (line-seq (java.io.BufferedReader. *in*))))
+  (take
+   number-of-lines
+   (line-seq (java.io.BufferedReader. *in*))))
 
 (defn- str->command [str-command]
   (let [[compass-direction number-of-steps] (str/split str-command #" ")]
     {:compass-direction (keyword compass-direction)
      :number-of-steps (Integer. number-of-steps)}))
 
-(defn read-all! []
+(defn read! []
   (let [number-of-lines                 (read-int-ln)
         [start-position & str-commands] (input->str-list number-of-lines)
         [start-x start-y]               (map #(Integer. %) (str/split start-position #" "))
@@ -20,3 +25,6 @@
     (if str-commands
       (assoc result :commands (map str->command str-commands))
       result)))
+
+(defn write! [positions]
+  (println (str "=> Cleaned: " positions)))
