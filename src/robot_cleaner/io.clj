0e692315e4efcadce6e51ab8f116b707fa04273e
gabriela <gabipilegi@gmail.com>
2020-02-26 01:15:33 -0300

    Read and parse input

--- src/robot_cleaner/io.clj
+++ src/robot_cleaner/io.clj
@@ -1,5 +1,22 @@
-(ns robot-cleaner.io)
+(ns robot-cleaner.io
+  (:require [clojure.string :as str]))
 
-(defn input-list! []
-  (let [number-of-lines (-> (read-line) Integer. int (+ 1))]
-    (take number-of-lines (line-seq (java.io.BufferedReader. *in*)))))
+(defn- read-int-ln []
+  (-> (read-line) Integer. int (+ 1)))
+
+(defn- input->str-list [number-of-lines]
+  (take number-of-lines (line-seq (java.io.BufferedReader. *in*))))
+
+(defn- str->command [str-command]
+  (let [[compass-direction number-of-steps] (str/split str-command #" ")]
+    {:compass-direction (keyword compass-direction)
+     :number-of-steps (Integer. number-of-steps)}))
+
+(defn read-all! []
+  (let [number-of-lines                 (read-int-ln)
+        [start-position & str-commands] (input->str-list number-of-lines)
+        [start-x start-y]               (map #(Integer. %) (str/split start-position #" "))
+        result {:start-position {:x start-x :y start-y}}]
+    (if str-commands
+      (assoc result :commands (map str->command str-commands))
+      result)))
--- test/robot_cleaner/io_test.clj
+++ test/robot_cleaner/io_test.clj
@@ -1,14 +1,37 @@
 (ns robot-cleaner.io-test
-  (:require [robot-cleaner.io :as io]
-            [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [robot-cleaner.io :as io]))
 
-(deftest input-list
-  (testing "reads input with no commands"
-    (is (= (with-in-str "0\n1 2\n" (io/input-list!))
-           ["1 2"])))
-  (testing "reads input with one command"
-    (is (= (with-in-str "1\n1 2\nE 1\n" (io/input-list!))
-           ["1 2" "E 1"])))
-  (testing "reads input with various commands"
-    (is (= (with-in-str "4\n1 2\nW 1\nE 2\nS 5\nN 4\n" (io/input-list!))
-           ["1 2" "W 1" "E 2" "S 5" "N 4"]))))
+(deftest read-all
+  (testing "Inputs with no commands"
+    (is (= {:start-position {:x 1 :y 2}}
+           (with-in-str "0\n1 2\n" (io/read-all!)))))
+  (testing "Inputs with one command"
+    (is (=  {:start-position {:x 3 :y 4}
+             :commands       [{:compass-direction :E
+                               :number-of-steps   1}]}
+            (with-in-str "1\n3 4\nE 1\n" (io/read-all!)))))
+
+  (testing "Inputs with various commands"
+    (is (= {:start-position {:x 4 :y 6}
+            :commands       [{:compass-direction :W
+                              :number-of-steps   1}
+                             {:compass-direction :E
+                              :number-of-steps   2}
+                             {:compass-direction :S
+                              :number-of-steps   5}
+                             {:compass-direction :N
+                              :number-of-steps   4}]}
+           (with-in-str "4\n4 6\nW 1\nE 2\nS 5\nN 4\n" (io/read-all!)))))
+
+  (testing "Inputs with maximum possible values"
+    (is (= {:start-position {:x 100 :y 100}
+            :commands       [{:compass-direction :E
+                              :number-of-steps   100}]}
+           (with-in-str "1\n100 100\nE 100\n" (io/read-all!)))))
+
+  (testing "Inputs with minimum possible values"
+    (is (= {:start-position {:x -100 :y -100}
+            :commands       [{:compass-direction :E
+                              :number-of-steps   -100}]}
+           (with-in-str "1\n-100 -100\nE -100\n" (io/read-all!))))))
