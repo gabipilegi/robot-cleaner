540e9ef7207b653f7b4a364bcf36886bf1dbd275
gabriela <gabipilegi@gmail.com>
2020-02-26 12:24:46 -0300

    Making test more legible

--- test/robot_cleaner/io_test.clj
+++ test/robot_cleaner/io_test.clj
@@ -3,16 +3,17 @@
             [robot-cleaner.io :as io]))
 
 (deftest read!
-  (testing "Inputs with no commands"
+  (testing "Input with no commands"
     (is (= {:start-position {:x 1 :y 2}}
            (with-in-str "0\n1 2\n" (io/read!)))))
-  (testing "Inputs with one command"
+
+  (testing "Input with one command"
     (is (=  {:start-position {:x 3 :y 4}
              :commands       [{:compass-direction :E
                                :number-of-steps   1}]}
             (with-in-str "1\n3 4\nE 1\n" (io/read!)))))
 
-  (testing "Inputs with various commands"
+  (testing "Input with various commands"
     (is (= {:start-position {:x 4 :y 6}
             :commands       [{:compass-direction :W
                               :number-of-steps   1}
@@ -24,13 +25,13 @@
                               :number-of-steps   4}]}
            (with-in-str "4\n4 6\nW 1\nE 2\nS 5\nN 4\n" (io/read!)))))
 
-  (testing "Inputs with maximum possible values"
+  (testing "Input with maximum possible values"
     (is (= {:start-position {:x 100 :y 100}
             :commands       [{:compass-direction :E
                               :number-of-steps   100}]}
            (with-in-str "1\n100 100\nE 100\n" (io/read!)))))
 
-  (testing "Inputs with minimum possible values"
+  (testing "Input with minimum possible values"
     (is (= {:start-position {:x -100 :y -100}
             :commands       [{:compass-direction :E
                               :number-of-steps   -100}]}
